ba81dc064c2d1f2c1bee625594901fee8b227a0f
Thomas Hisch <thomas@opentech.at>
2010-12-27 22:06:40 +0100

    [emacs] color theme:  autoloading, subdirs to load-path
    Signed-off-by: Thomas Hisch <thomas@opentech.at>

--- .emacs.d/init-theme.el
+++ .emacs.d/init-theme.el
@@ -1,11 +1,19 @@
 (require 'color-theme)
 (setq color-theme-load-all-themes nil)
-(add-to-list 'load-path "~/.emacs.d/colors")
-;;(require 'color-theme-tango-2)
-;;(color-theme-tango-2)
-;;(require 'color-theme-tangotango)
-;;(color-theme-tangotango)
-(load-file "~/.emacs.d/colors/color-theme-wombat/color-theme-wombat.el")
+
+;; add all subdirs of colors-dir to loadpath
+(let* ((my-colors-dir "~/.emacs.d/colors/")
+       (default-directory my-colors-dir)
+       (orig-load-path load-path))
+  (setq load-path (cons my-colors-dir nil))
+  (normal-top-level-add-subdirs-to-load-path)
+  (nconc load-path orig-load-path))
+
+(autoload 'color-theme-tango-2 "color-theme-tango-2" nil t)
+(autoload 'color-theme-tangotango "color-theme-tangotango" nil t)
+(autoload 'color-theme-zenburn "zenburn" nil t)
+(autoload 'color-theme-wombat "color-theme-wombat" nil t)
+
 (color-theme-wombat)
 
 (require 'diff-mode)
