648d8e02e765f6caa2a5c9fade6fe3075bcdd8bb
Thomas Hisch <thomas@opentech.at>
2011-01-03 02:08:39 +0100

    [emacs] don't load tramp inside ido
    Signed-off-by: Thomas Hisch <thomas@opentech.at>

--- .emacs.d/custom.el
+++ .emacs.d/custom.el
@@ -3,6 +3,7 @@
   ;; If you edit it by hand, you could mess it up, so be careful.
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
+ '(ido-enable-tramp-completion nil)
  '(viper-vi-state-mode-list (quote (fundamental-mode makefile-mode awk-mode m4-mode xrdb-mode winmgr-mode autoconf-mode cvs-edit-mode html-mode html-helper-mode emacs-lisp-mode lisp-mode lisp-interaction-mode jde-mode java-mode cc-mode c-mode c++-mode objc-mode fortran-mode f90-mode basic-mode bat-mode asm-mode prolog-mode flora-mode sql-mode text-mode indented-text-mode tex-mode latex-mode bibtex-mode ps-mode diff-mode idl-mode perl-mode cperl-mode javascript-mode tcl-mode python-mode sh-mode ksh-mode csh-mode gnus-article-mode mh-show-mode ada-mode))))
 (custom-set-faces
   ;; custom-set-faces was added by Custom.
