bd3b1efdc6f30256a7eb32428121e11b9f4db0f9
Thomas Hisch <thomas@opentech.at>
2011-01-03 18:41:59 +0100

    replace whoami with $user
    Signed-off-by: Thomas Hisch <thomas@opentech.at>

--- .emacs.d/start-emacs-server.sh
+++ .emacs.d/start-emacs-server.sh
@@ -1,5 +1,5 @@
 #/bin/dash
 
 date >> ~/.emacs.d/emacs-server.log
-pgrep -u `whoami` emacs > /dev/null || emacs --daemon &>> ~/.emacs.d/emacs-server.log
+pgrep -u $USER emacs > /dev/null || emacs --daemon &>> ~/.emacs.d/emacs-server.log
 
