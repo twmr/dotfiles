19b0dbbefcaeb526a1a84c36ea7f9fb4d02ffec2
Thomas Hisch <thomas@opentech.at>
2011-01-18 12:20:14 +0100

    [emacs] org-cycle in latex for structured editing
    Signed-off-by: Thomas Hisch <thomas@opentech.at>

--- .emacs.d/thi/latex.el
+++ .emacs.d/thi/latex.el
@@ -4,3 +4,21 @@
 (setq TeX-parse-self t)
 ;; make auctex aware of the multi-file document structure
 (setq-default TeX-master nil)
+
+
+(defun turn-on-outline-minor-mode ()
+  (outline-minor-mode 1))
+
+(require 'org-install)
+(add-hook 'LaTeX-mode-hook 'turn-on-outline-minor-mode)
+(add-hook 'latex-mode-hook 'turn-on-outline-minor-mode)
+;;(setq outline-minor-mode-prefix "\C-c\C-o") ; Or something else
+
+(add-hook 'outline-minor-mode-hook
+          (lambda ()
+            (define-key outline-minor-mode-map [(control tab)] 'org-cycle)
+            (define-key outline-minor-mode-map [(shift tab)] 'org-global-cycle)))
+(add-hook 'outline-mode-hook
+          (lambda ()
+            (define-key outline-mode-map [(tab)] 'org-cycle)
+            (define-key outline-mode-map [(shift tab)] 'org-global-cycle)))
