25a651c1dbee9f7af7f6af24ad7b78a185fb7f48
Thomas Hisch <thomas@opentech.at>
2011-01-27 01:12:36 +0100

    [emacs] color customizations and tabbar once again activated

--- .emacs.d/thi/custom.el
+++ .emacs.d/thi/custom.el
@@ -3,9 +3,11 @@
   ;; If you edit it by hand, you could mess it up, so be careful.
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
+ '(fringe-mode (quote (5 . 5)) nil (fringe))
  '(ido-enable-tramp-completion nil)
  '(ido-use-faces t)
  '(tabbar-background-color "black")
+ '(tabbar-mode t nil (tabbar))
  '(tabbar-use-images nil)
  '(viper-vi-state-mode-list (quote (fundamental-mode makefile-mode awk-mode m4-mode xrdb-mode winmgr-mode autoconf-mode cvs-edit-mode html-mode html-helper-mode emacs-lisp-mode lisp-mode lisp-interaction-mode jde-mode java-mode cc-mode c-mode c++-mode objc-mode fortran-mode f90-mode basic-mode bat-mode asm-mode prolog-mode flora-mode sql-mode text-mode indented-text-mode tex-mode latex-mode bibtex-mode ps-mode diff-mode idl-mode perl-mode cperl-mode javascript-mode tcl-mode python-mode sh-mode ksh-mode csh-mode gnus-article-mode mh-show-mode ada-mode))))
 (custom-set-faces
@@ -15,4 +17,7 @@
   ;; If there is more than one, they won't work right.
  '(ido-first-match ((t (:foreground "#ad7f8a" :weight bold))))
  '(ido-subdir ((((min-colors 88) (class color)) (:foreground "DarkGoldenrod1"))))
- '(tabbar-button ((t (:foreground "white")))))
+ '(tabbar-button ((t (:foreground "#ad7f8a"))))
+ '(tabbar-selected ((t (:background "DarkGoldenrod1" :foreground "Black"))))
+ '(tabbar-separator ((t (:height 0.5))))
+ '(tabbar-unselected ((t nil))))
