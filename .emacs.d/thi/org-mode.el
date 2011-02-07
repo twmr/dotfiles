bdb1ba7f24939cdaa4ef628c3bb3e675fda5aa75
Thomas Hisch <thomas@opentech.at>
2011-02-08 00:56:02 +0100

    [emacs] org mode agenda files
    Signed-off-by: Thomas Hisch <thomas@opentech.at>

--- .emacs.d/thi/org-mode.el
+++ .emacs.d/thi/org-mode.el
@@ -1,9 +1,13 @@
 (set-face-attribute 'org-table nil :inherit 'fixed-pitch)
 (set-face-attribute 'org-block nil :inherit 'fixed-pitch)
 
+(setq org-agenda-files (quote ("~/Dropbox/notes-org/studium.org"
+                               "~/Dropbox/notes-org/emacs.org")))
+
 ;; fontify src blocks, doesn't work with fixed-pitch blocks :(
 ;;(setq org-src-fontify-natively t)
 
+;; (setq org-hide-leading-stars t)
 (setq org-use-speed-commands t)
 (setq org-speed-commands-user (quote (("0" . delete-window)
                                       ("1" . delete-other-windows)
@@ -17,3 +21,8 @@
                                       ("c" . self-insert-command)
                                       ("C" . self-insert-command)
                                       ("J" . org-clock-goto))))
+;;
+;; Standard key bindings
+(global-set-key "\C-cl" 'org-store-link)
+(global-set-key "\C-ca" 'org-agenda)
+(global-set-key "\C-cb" 'org-iswitchb)
