b42f2c40f9d653362d0f21a6bcb87c81fccf3bc1
Thomas Hisch <thomas@opentech.at>
2011-03-30 15:56:41 +0200

    [emacs] reftex hook in org-mode
    Signed-off-by: Thomas Hisch <thomas@opentech.at>

--- .emacs.d/thi/org-mode.el
+++ .emacs.d/thi/org-mode.el
@@ -29,3 +29,11 @@
 (global-set-key "\C-cl" 'org-store-link)
 (global-set-key "\C-ca" 'org-agenda)
 (global-set-key "\C-cb" 'org-iswitchb)
+
+(defun org-mode-reftex-setup ()
+  (load-library "reftex")
+  (and (buffer-file-name)
+       (file-exists-p (buffer-file-name))
+       (reftex-parse-all))
+  (define-key org-mode-map (kbd "C-c )") 'reftex-citation))
+(add-hook 'org-mode-hook 'org-mode-reftex-setup)
