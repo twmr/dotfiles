0957194a0ccdc3e72c1fa151688712e93db85426
Thomas Hisch <thomas@opentech.at>
2011-05-16 16:36:56 +0200

    [emacs] add magit-key-mode to viper-emacs-state-mode-list to fix broken git push in magit

--- .emacs.d/thi/custom.el
+++ .emacs.d/thi/custom.el
@@ -12,6 +12,7 @@
  '(tabbar-background-color "black")
  '(tabbar-mode t nil (tabbar))
  '(tabbar-use-images nil)
+ '(viper-emacs-state-mode-list (quote (Custom-mode dired-mode efs-mode tar-mode browse-kill-ring-mode recentf-mode recentf-dialog-mode occur-mode mh-folder-mode gnus-group-mode gnus-summary-mode completion-list-mode help-mode Info-mode Buffer-menu-mode compilation-mode rcirc-mode jde-javadoc-checker-report-mode view-mode vm-mode vm-summary-mode magit-key-mode)))
  '(viper-vi-state-mode-list (quote (fundamental-mode makefile-mode awk-mode m4-mode xrdb-mode winmgr-mode autoconf-mode cvs-edit-mode html-mode html-helper-mode emacs-lisp-mode lisp-mode lisp-interaction-mode jde-mode java-mode cc-mode c-mode c++-mode objc-mode fortran-mode f90-mode basic-mode bat-mode asm-mode prolog-mode flora-mode sql-mode text-mode indented-text-mode tex-mode latex-mode bibtex-mode ps-mode diff-mode idl-mode perl-mode cperl-mode javascript-mode tcl-mode python-mode sh-mode ksh-mode csh-mode gnus-article-mode mh-show-mode ada-mode))))
 (custom-set-faces
   ;; custom-set-faces was added by Custom.
