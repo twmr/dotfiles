7092e269d22a8a4439f2ae9e253549316871d406
Thomas Hisch <thomas@opentech.at>
2011-06-01 00:57:34 +0200

    [emacs] fix: fully toogle file sections

--- .emacs.d/thi/magit.el
+++ .emacs.d/thi/magit.el
@@ -16,13 +16,20 @@
 
 (eval-after-load 'magit
   '(progn
-;;     (set-face-foreground 'magit-diff-add "green3")
-;;     (set-face-foreground 'magit-diff-del "red3")
-;;     (set-face-background 'magit-item-highlight "gray15")
      (define-key magit-mode-map (kbd "M-3") 'split-window-horizontally) ; was magit-show-level-3
      (define-key magit-mode-map (kbd "M-2") 'split-window-vertically)   ; was magit-show-level-2
      (define-key magit-mode-map (kbd "M-1") 'delete-other-windows)      ; was magit-show-level-1
      (defun magit-pull ()
        (interactive)
        (magit-run-git-async "pull" "--rebase" "-v"))
-     ))
+     (defun magit-toggle-section () ;; overwrite magit-toggle-section function
+       "Toggle hidden status of current section."
+       (interactive)
+       (if (eq 'hunk (first (magit-section-context-type (magit-current-section))))
+           (magit-toggle-file-section)
+         (magit-section-hideshow
+          (lambda (s)
+            (magit-section-set-hidden s (not (magit-section-hidden s)))))))
+     )
+
+  )
