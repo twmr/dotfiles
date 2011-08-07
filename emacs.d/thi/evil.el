0eda25151a3ac40b86963f8718c6bd8d6cdbd970
Thomas Hisch <thomas@opentech.at>
2011-08-07 23:33:00 +0200

    [emacs] evil - magit stuff

--- emacs.d/thi/evil.el
+++ emacs.d/thi/evil.el
@@ -5,3 +5,10 @@
 ;; DISABLE hl-line in visual mode:
 (add-hook 'evil-visual-state-entry-hook 'thi-turn-hl-line-mode-off)
 (add-hook 'evil-visual-state-exit-hook 'thi-turn-hl-line-mode-on)
+
+;; associate certain modes with default evil states
+(add-to-list 'evil-emacs-state-modes 'magit-mode)
+
+;; extra keybindings
+
+(define-key evil-normal-state-map "gs" 'magit-status)
