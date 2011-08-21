cfbfeb0bbe31fd4ad06765b8be82da13fa572c8e
Thomas Hisch <thomas@opentech.at>
2011-08-21 19:23:02 +0200

    [emacs] evil modeline changes (commented out) added
    I also need to change the foreground colors, in order to use the new
    modlinecolor stuff

--- emacs.d/thi/evil.el
+++ emacs.d/thi/evil.el
@@ -2,6 +2,37 @@
 
 ;;TODO modeline color changes
 
+;; (defvar evil-normal-mode-line-background (face-background 'mode-line))
+;; (defvar evil-normal-mode-line-foreground (face-foreground 'mode-line))
+;; (defun evil-set-mode-line-color (&rest after-which-mode)
+;;   (set-face-background
+;;    'mode-line
+;;    (cond
+;;     ((evil-visual-state-p) "cornflower blue")
+;;     ((evil-emacs-state-p) evil-normal-mode-line-background)
+;;     ((evil-visual-state-p) "cornflower blue")
+;;     ((evil-normal-state-p) "DarkGoldenrod1"))
+;;    ))
+
+
+;;   ;; (set-face-foreground
+;;   ;;  'mode-line
+;;   ;;  (cond
+;;   ;;   ((eq viper-current-state 'replace-state)
+;;   ;;    "black")
+;;   ;;   ((eq viper-current-state 'emacs-state)
+;;   ;;    evil-normal-mode-line-foreground)
+;;   ;;   ((eq viper-current-state 'insert-state)
+;;   ;;    "black")
+;;   ;;   ((eq viper-current-state 'vi-state)
+;;   ;;    "black"))))
+
+;; (add-hook 'evil-normal-state-entry-hook 'evil-set-mode-line-color)
+;; (add-hook 'evil-visual-state-entry-hook 'evil-set-mode-line-color)
+;; (add-hook 'evil-emacs-state-entry-hook 'evil-set-mode-line-color)
+;; (add-hook 'window-configuration-change-hook 'evil-set-mode-line-color)
+
+
 ;; DISABLE hl-line in visual mode:
 (add-hook 'evil-visual-state-entry-hook 'thi-turn-hl-line-mode-off)
 (add-hook 'evil-visual-state-exit-hook 'thi-turn-hl-line-mode-on)
