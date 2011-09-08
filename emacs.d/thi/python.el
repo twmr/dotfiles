7959f89b2bf55731631410525e7730e47e28a82a
Thomas Hisch <thomas@opentech.at>
2011-09-08 17:57:28 +0200

    [emacs] python.el file add
    Signed-off-by: Thomas Hisch <thomas@opentech.at>

---
+++ emacs.d/thi/python.el
@@ -0,0 +1,9 @@
+(setq
+ python-shell-interpreter "ipython"
+ python-shell-interpreter-args ""
+ python-shell-prompt-regexp "In \\[[0-9]+\\]: "
+ python-shell-prompt-output-regexp "Out\\[[0-9]+\\]: "
+ python-shell-completion-setup-code ""
+ python-shell-completion-string-code
+ "';'.join(get_ipython().Completer.all_completions('''%s'''))\n"
+ python-skeleton-autoinsert t )
