db3577f755433c3c33e6ac5ee30e141b9de5097c
Thomas Hisch <thomas@opentech.at>
2011-09-08 23:01:24 +0200

    [emacs] *.markdown files open in markdown mode
    Signed-off-by: Thomas Hisch <thomas@opentech.at>

--- emacs.d/thi/global.el
+++ emacs.d/thi/global.el
@@ -113,7 +113,7 @@
         ("\\.asm\\'" . asm-mode)
         ("\\.F90\\'" . f90-mode)
         ("\\.m\\'" . matlab-mode)
-        ("\\.md\\'" . markdown-mode)
+        ("\\.\\(md\\|markdown\\)\\'" . markdown-mode)
         ("ChangeLog\\'" . change-log-mode)
         ("change\\.log\\'" . change-log-mode)
         ("changelo\\'" . change-log-mode)
