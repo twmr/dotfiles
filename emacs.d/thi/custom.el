89200f3db776b71fd33c3260cd5c3195a3cd684f
Thomas Hisch <thomas@opentech.at>
2011-09-08 12:44:13 +0200

    [emacs] custom.el update (auctex and evil)
    Signed-off-by: Thomas Hisch <thomas@opentech.at>

--- emacs.d/thi/custom.el
+++ emacs.d/thi/custom.el
@@ -1,8 +1,9 @@
 (custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
+  ;; custom-set-variables was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ '(evil-mode-line-format (quote before))
  '(fringe-mode (quote (5 . 5)) nil (fringe))
  '(ido-enable-tramp-completion nil)
  '(ido-use-faces t)
@@ -10,13 +11,15 @@
  '(magit-status-buffer-switch-function (quote switch-to-buffer))
  '(matlab-indent-level 2)
  '(org-file-apps (quote ((auto-mode . emacs) ("\\.mm\\'" . default) ("\\.x?html?\\'" . default) ("pdf" . "evince %s"))))
+ '(preview-scale-function 1.5)
+ '(preview-transparent-color (quote (highlight :background)))
  '(viper-emacs-state-mode-list (quote (Custom-mode dired-mode efs-mode tar-mode browse-kill-ring-mode recentf-mode recentf-dialog-mode occur-mode mh-folder-mode gnus-group-mode gnus-summary-mode completion-list-mode help-mode Info-mode Buffer-menu-mode compilation-mode rcirc-mode jde-javadoc-checker-report-mode view-mode vm-mode vm-summary-mode magit-key-mode)))
  '(viper-vi-state-mode-list (quote (fundamental-mode makefile-mode awk-mode m4-mode xrdb-mode winmgr-mode autoconf-mode cvs-edit-mode html-mode html-helper-mode emacs-lisp-mode lisp-mode lisp-interaction-mode jde-mode java-mode cc-mode c-mode c++-mode objc-mode fortran-mode f90-mode basic-mode bat-mode asm-mode prolog-mode flora-mode sql-mode text-mode indented-text-mode tex-mode latex-mode bibtex-mode ps-mode diff-mode idl-mode perl-mode cperl-mode javascript-mode tcl-mode python-mode sh-mode ksh-mode csh-mode gnus-article-mode mh-show-mode ada-mode))))
 (custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
+  ;; custom-set-faces was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
  '(ido-first-match ((t (:foreground "#ad7f8a" :weight bold))))
  '(ido-subdir ((((min-colors 88) (class color)) (:foreground "DarkGoldenrod1")))))
  ;; '(tabbar-button ((t (:foreground "#ad7f8a"))))
