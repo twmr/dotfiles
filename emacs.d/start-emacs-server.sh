620813884ec6764094034f765c7fd68eb9448bb2
Thomas Hisch <thomas@opentech.at>
2012-01-05 10:54:48 +0100

    use bash instead of dash in emacs-daemon script
    Signed-off-by: Thomas Hisch <thomas@opentech.at>

--- emacs.d/start-emacs-server.sh
+++ emacs.d/start-emacs-server.sh
@@ -1,5 +1,5 @@
-#/bin/dash
+#/bin/bash
 
 date >> ~/.emacs.d/emacs-server.log
-pgrep -u $USER emacs > /dev/null || emacs --daemon &>> ~/.emacs.d/emacs-server.log
+pgrep -u $USER emacs > /dev/null || emacs --daemon >> ~/.emacs.d/emacs-server.log 2>&1
 
