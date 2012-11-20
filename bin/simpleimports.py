92820a8f53e01a0f79284549579afdeccc593e6f
Thomas Hisch <t.hisch@gmail.com>
2012-11-20 17:56:13 +0100

    simpleimport up

--- bin/simpleimports.py
+++ bin/simpleimports.py
@@ -4,6 +4,7 @@ import _hwlib.multipole as MP
 import _hwlib.powersupply as PS
 import _hwlib.ramper as RP
 import _hwlib.scanner as SC
+import _hwlib.cup as CP
 
 print "loading ObjectTree"
 otree = hwlib.loadTree(hwlib.servers)
@@ -17,4 +18,7 @@ ramper = RP.Ramper('RAMPER_PSYS_C')
 mp2 = MP.Multipole('MP2')
 
 scanner = SC.TestScanner('ScannerNew')
-print "ramper,psu, scanner and mp2 objects initialized"
+
+cup  = CP('PAM_FC1')
+
+print "ramper, cup, psu, scanner and mp2 objects initialized"
