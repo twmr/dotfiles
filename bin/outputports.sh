87366b6fdb89379851195ad15b97efd208369b67
Thomas Hisch <t.hisch@gmail.com>
2012-11-21 15:48:47 +0100

    outputports script fix

--- bin/outputports.sh
+++ bin/outputports.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-
 echo "Port of active NetLib Servers"
-grep "Binding service" *.log | awk -F ':' '{ print $8 }' | sort -n
+grep "Binding service" *.log | awk -F '*' '{ print $2 }' | cut -d: -f2 | sort -n
