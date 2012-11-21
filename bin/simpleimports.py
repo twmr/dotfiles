98ac28e6e1872cc773cf0d0f73a6f092cf4f6630
Thomas Hisch <t.hisch@gmail.com>
2012-11-21 09:38:05 +0100

    cleanup up simpleimport script

--- bin/simpleimports.py
+++ bin/simpleimports.py
@@ -1,24 +1,24 @@
 from _hwlib import hwlib
-from _hwlib import pluginmanager #sonst funktioniert hwlib.loadTree nicht !!
-import _hwlib.multipole as MP
-import _hwlib.powersupply as PS
-import _hwlib.ramper as RP
-import _hwlib.scanner as SC
-import _hwlib.cup as CP
+import _hwlib .pluginmanager #sonst funktioniert hwlib.loadTree nicht !!
+import _hwlib.multipole
+import _hwlib.powersupply
+import _hwlib.ramper
+import _hwlib.scanner
+import _hwlib.cup
+import _hwlib.ramper
 
 print "loading ObjectTree"
 otree = hwlib.loadTree(hwlib.servers)
 print "Done"
 
-psu = PS.PowerSupply('PSU_HVREF')
+psu = hwlib.PowerSupply('PSU_HVREF')
 
-from _hwlib import ramper as RP
-ramper = RP.Ramper('RAMPER_PSYS_C')
+ramper = hwlib.Ramper('RAMPER_PSYS_C')
 
-mp2 = MP.Multipole('MP2')
+mp2 = hwlib.Multipole('MP2')
 
-scanner = SC.TestScanner('ScannerNew')
+scanner = hwlib.TestScanner('ScannerNew')
 
-cup  = CP('PAM_FC1')
+cup  = hwlib.Cup('PAM_FC1')
 
 print "ramper, cup, psu, scanner and mp2 objects initialized"
