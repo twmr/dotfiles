a2d20aff112d69ab8985cdac0d6ce3f87537396a
Thomas Hisch <t.hisch@gmail.com>
2012-11-23 17:05:02 +0100

    ups

--- bin/simpleimports.py
+++ bin/simpleimports.py
@@ -11,14 +11,14 @@ print "loading ObjectTree"
 otree = hwlib.loadTree(hwlib.servers)
 print "Done"
 
-psu = hwlib.PowerSupply('PSU_HVREF')
+psu = hwlib.PowerSupply('PSU_HVREF', verbose=True)
 
-ramper = hwlib.Ramper('RAMPER_PSYS_C')
+ramper = hwlib.Ramper('RAMPER_PSYS_C', verbose=True)
 
-mp2 = hwlib.Multipole('MP2')
+mp2 = hwlib.Multipole('MP2', verbose=True)
 
-scanner = hwlib.TestScanner('ScannerNew')
+scanner = hwlib.TestScanner('SCANNER1', verbose=True)
 
-cup  = hwlib.Cup('PAM_FC1')
+cup  = hwlib.Cup('PAM_FC1',verbose=True)
 
 print "ramper, cup, psu, scanner and mp2 objects initialized"
