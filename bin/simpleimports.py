aa26aa4778e5850d81485dac95ae02b48c4c3ac6
Thomas Hisch <t.hisch@gmail.com>
2012-11-26 14:44:55 +0100

    use the readqueen server in simpleimports

--- bin/simpleimports.py
+++ bin/simpleimports.py
@@ -1,15 +1,10 @@
 from _hwlib import hwlib
-import _hwlib .pluginmanager #sonst funktioniert hwlib.loadTree nicht !!
 import _hwlib.multipole
 import _hwlib.powersupply
 import _hwlib.ramper
 import _hwlib.scanner
 import _hwlib.cup
 import _hwlib.ramper
-
-print "loading ObjectTree"
-otree = hwlib.loadTree(hwlib.servers)
-print "Done"
 
 psu = hwlib.PowerSupply('PSU_HVREF', verbose=True)
 
