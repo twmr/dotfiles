594fd704d3f6067b3952dd6f24a7c187974b87fe
Thomas Hisch <t.hisch@gmail.com>
2013-01-17 20:40:29 +0100

    json mode syntax error fix
    Signed-off-by: Thomas Hisch <t.hisch@gmail.com>

--- emacs.d/thi/global.el
+++ emacs.d/thi/global.el
@@ -50,6 +50,26 @@
 ;;default font is now set in .Xdefaults
 ;;(set-default-font "ProFont-9")
 
+;; JSON MODE FIX
+
+(defadvice js2-reparse (before json)
+  (setq js2-buffer-file-name buffer-file-name))
+(ad-activate 'js2-reparse)
+
+(defadvice js2-parse-statement (around json)
+  (if (and (= tt js2-LC)
+      js2-buffer-file-name
+      (string-equal (substring js2-buffer-file-name -5) ".json")
+      (eq (+ (save-excursion
+            (goto-char (point-min))
+            (back-to-indentation)
+            (while (eolp)
+              (next-line)
+              (back-to-indentation))
+            (point)) 1) js2-ts-cursor))
+    (setq ad-return-value (js2-parse-assign-expr))
+    ad-do-it))
+(ad-activate 'js2-parse-statement)
 
 ;:::::::::::::::::::::::::::::::::::::::::::::::
 ;: Logical Behavour
