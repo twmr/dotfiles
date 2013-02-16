4b64e0400487fc89b3cd0e4efb5f8a6f4d6bf325
Thomas Hisch <t.hisch@gmail.com>
2013-02-16 11:22:04 +0100

    [emacs] disable rainbow stuff

--- emacs.d/thi/global.el
+++ emacs.d/thi/global.el
@@ -183,8 +183,9 @@
       '(:box (:color "grey10" :line-width 2) :background "red" :bold t :foreground "yellow")
       prepend)))
   ;; (idle-highlight-mode 1)
-  (rainbow-mode 1)
-  (rainbow-delimiters-mode 1)
+  ;; temporarily disabled the rainbow modes as i think they cause speed problems
+  ;; (rainbow-mode 1)
+  ;; (rainbow-delimiters-mode 1)
   (setq show-trailing-whitespace t)
   ;; (flyspell-prog-mode)
   )
