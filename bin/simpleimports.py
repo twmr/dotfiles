da26d0e09f8d45afa682840af81a5dda1d79a286
Thomas Hisch <t.hisch@gmail.com>
2013-02-22 11:49:44 +0100

    simpleupdates update

--- bin/simpleimports.py
+++ bin/simpleimports.py
@@ -13,6 +13,7 @@ ramper = hwlib.Ramper('RAMPER_PSYS_C', verbose=True)
 mp2 = hwlib.Multipole('MP2', verbose=True)
 
 scanner = hwlib.Scanner('SCANNER1', verbose=True)
+scanner2 = hwlib.Scanner('SCANNER2', verbose=True)
 
 cup = hwlib.Cup('PAM_FC1',verbose=True)
 
@@ -20,4 +21,6 @@ session = SessionManager('SessionManager', verbose=True)
 
 mp5 = Base('MP5', verbose=True)
 
-print "ramper, cup, psu, scanner, session, mp2 and mp5 objects initialized"
+exp = Base('POC_Exposure', verbose=True)
+
+print "ramper, cup, psu, scanner, scanner2, session, mp2 and mp5 objects initialized"
