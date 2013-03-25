8f94c7efcca472ec8ea7c3bb5cad207633f32132
Thomas Hisch <t.hisch@gmail.com>
2013-03-25 22:09:01 +0100

    dirs in project.el up

--- emacs.d/thi/projects.el
+++ emacs.d/thi/projects.el
@@ -1,16 +1,21 @@
-(defvar thi::directory-list '("~/gitrepos/dotfiles/emacs.d"
-                              "~/gitrepos/dotfiles/emacs.d/thi"
-                              "~/gitrepos/dotfiles/emacs.d/el-get"
+(defvar thi::directory-list '(
                               "/opt/sandboxes"
-                              "/opt/sandboxes/trunk"
+                              "/opt/sandboxes/current-branch"
                               "/opt/sandboxes/poc4imo"
+
+                              "~/gitrepos/POC-scripts/trunk/poclib"
+                              "~/gitrepos/POC-scripts/trunk/tests"
+                              "~/gitrepos/POC-scripts/trunk/integrationtests"
+
+                              "~/gitrepos/tools"
+                              "~/gitrepos/tools/future-tools"
+
                               "~/gitrepos/imshelperscripts"
                               "~/gitrepos/HWSimuEnv"
-                              "~/gitrepos/tools"
-                              "~/gitrepos/POC-scripts/branches/POC4IMO"
-                              "~/gitrepos/pocscripts/branches/POC4IMO"
-                              "/opt/imsrepos"
-                              "/opt/imsblobs"
+
+                              "~/gitrepos/dotfiles/emacs.d"
+                              "~/gitrepos/dotfiles/emacs.d/thi"
+                              "~/gitrepos/dotfiles/emacs.d/el-get"
                               ))
 
 (require 'cl)
