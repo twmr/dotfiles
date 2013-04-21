c7f4b2020f05a34d9411106c96dd9e14964ecbb5
Thomas Hisch <t.hisch@gmail.com>
2013-04-21 18:29:41 +0200

    project list update

--- emacs.d/thi/smart-mode-line.conf.el
+++ emacs.d/thi/smart-mode-line.conf.el
@@ -2,8 +2,8 @@
 (add-to-list 'sml/replacer-regexp-list '("^:DB:Documents" ":DDocs:"))
 (add-to-list 'sml/replacer-regexp-list '("^~/Dropbox/" ":DB:"))
 (add-to-list 'sml/replacer-regexp-list '("^~/gitrepos/" ":Git:"))
-(add-to-list 'sml/replacer-regexp-list '("^~/gitrepos/POC-scripts" ":pst:"))
-(add-to-list 'sml/replacer-regexp-list '("^~/gitrepos/pocscripts" ":pst:"))
+(add-to-list 'sml/replacer-regexp-list '("^~/gitrepos/POC-scripts/trunk" ":pst:"))
+(add-to-list 'sml/replacer-regexp-list '("^~/gitrepos/POC-scripts/branches/POC4IMO" ":psb"))
 (add-to-list 'sml/replacer-regexp-list '("^~/gitrepos/dotfiles/emacs.d" ":ED:"))
 (add-to-list 'sml/replacer-regexp-list '("^~/.emacs.d" ":ED:"))
 
