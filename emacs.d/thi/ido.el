7e74a140a8c77c0af1d99938c613e9a41b48d4a4
Thomas Hisch <t.hisch@gmail.com>
2013-04-27 19:33:09 +0200

    [emacs] ido sort by mtime

--- emacs.d/thi/ido.el
+++ emacs.d/thi/ido.el
@@ -53,3 +53,21 @@
   (define-key ido-completion-map (kbd "<backtab>") 'ido-complete) ;; use \C-g to exit minibuffer
   )
 (add-hook 'ido-setup-hook 'ido-my-keys)
+
+;; sort ido filelist by mtime instead of alphabetically
+(add-hook 'ido-make-file-list-hook 'ido-sort-mtime)
+(add-hook 'ido-make-dir-list-hook 'ido-sort-mtime)
+(defun ido-sort-mtime ()
+  (setq ido-temp-list
+        (sort ido-temp-list
+              (lambda (a b)
+                (time-less-p
+                 (sixth
+                  (file-attributes (concat ido-current-directory b)))
+                 (sixth
+                  (file-attributes (concat ido-current-directory a)))))))
+  (ido-to-end  ;; move . files to end (again)
+   (delq nil (mapcar
+              (lambda (x) (and (char-equal
+                                (string-to-char x) ?.) x))
+              ido-temp-list))))
