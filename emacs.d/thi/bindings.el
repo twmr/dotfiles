77079c62bc055a292108c9f730d99b4ebe5d5909
Thomas Hisch <t.hisch@gmail.com>
2013-05-28 01:01:56 +0200

    [emacs] open file at cursor

--- emacs.d/thi/bindings.el
+++ emacs.d/thi/bindings.el
@@ -15,6 +15,7 @@
 (global-set-key [f4] 'query-replace)
 (global-set-key [f5] 'magit-grep)
 (global-set-key [f6] 'magit-status)
+(global-set-key [f7] 'open-file-at-cursor)
 
 (global-set-key (kbd "C-x g") 'magit-status)
 
--- emacs.d/thi/defuns.el
+++ emacs.d/thi/defuns.el
@@ -101,3 +101,25 @@
         (other-window 1)
         (ansi-term (getenv "SHELL")))
     (switch-to-buffer-other-window "*ansi-term*")))
+
+
+(defun open-file-at-cursor ()
+    "Open the file path under cursor.
+If there is text selection, uses the text selection for path.
+If the path is starts with “http://”, open the URL in browser.
+Input path can be {relative, full path, URL}.
+This command is similar to `find-file-at-point' but without prompting for confirmation.
+"
+    (interactive)
+    (let ( (path (if (region-active-p)
+                     (buffer-substring-no-properties (region-beginning) (region-end))
+                   (thing-at-point 'filename) ) ))
+      (if (string-match-p "\\`https?://" path)
+          (browse-url path)
+        (progn ; not starting “http://”
+          (if (file-exists-p path)
+              (find-file path)
+            (if (file-exists-p (concat path ".el"))
+                (find-file (concat path ".el"))
+              (when (y-or-n-p (format "file doesn't exist: 「%s」. Create?" path) )
+                (find-file path )) ) ) ) ) ))
