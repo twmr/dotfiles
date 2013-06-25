f75b1b152488c5d13307df1c99b7f5949a9526a3
Thomas Hisch <t.hisch@gmail.com>
2013-06-25 21:49:25 +0200

    [emacs] fix ido-find-file problem (root directory)

--- emacs.d/thi/ido.el
+++ emacs.d/thi/ido.el
@@ -54,20 +54,22 @@
   )
 (add-hook 'ido-setup-hook 'ido-my-keys)
 
+;; With this code it is not possible anymore to go to the root directory with ido-find-file:  C-x C-f //  leads to a tramp error ?!?!?
+
 ;; sort ido filelist by mtime instead of alphabetically
-(add-hook 'ido-make-file-list-hook 'ido-sort-mtime)
-(add-hook 'ido-make-dir-list-hook 'ido-sort-mtime)
-(defun ido-sort-mtime ()
-  (setq ido-temp-list
-        (sort ido-temp-list
-              (lambda (a b)
-                (time-less-p
-                 (sixth
-                  (file-attributes (concat ido-current-directory b)))
-                 (sixth
-                  (file-attributes (concat ido-current-directory a)))))))
-  (ido-to-end  ;; move . files to end (again)
-   (delq nil (mapcar
-              (lambda (x) (and (char-equal
-                                (string-to-char x) ?.) x))
-              ido-temp-list))))
+;; (add-hook 'ido-make-file-list-hook 'ido-sort-mtime)
+;; (add-hook 'ido-make-dir-list-hook 'ido-sort-mtime)
+;; (defun ido-sort-mtime ()
+;;   (setq ido-temp-list
+;;         (sort ido-temp-list
+;;               (lambda (a b)
+;;                 (time-less-p
+;;                  (sixth
+;;                   (file-attributes (concat ido-current-directory b)))
+;;                  (sixth
+;;                   (file-attributes (concat ido-current-directory a)))))))
+;;   (ido-to-end  ;; move . files to end (again)
+;;    (delq nil (mapcar
+;;               (lambda (x) (and (char-equal
+;;                                 (string-to-char x) ?.) x))
+;;               ido-temp-list))))
