e47076dc07abf905b847ca5480119bea5aa72b3b
Thomas Hisch <t.hisch@gmail.com>
2013-07-22 09:43:26 +0200

    slepc petsc script up
    Signed-off-by: Thomas Hisch <t.hisch@gmail.com>

--- bin/petscslepc.py
+++ bin/petscslepc.py
@@ -8,15 +8,14 @@ from subprocess import check_call
 petscdir = os.getenv('PETSC_DIR')
 petsc4pydir = os.getenv('PETSC4PY_DIR')
 slepcdir = os.getenv('SLEPC_DIR')
-slep4pydir = os.getenv('SLEPC4PY_DIR')
-import ipdb; ipdb.set_trace()
-
+slepc4pydir = os.getenv('SLEPC4PY_DIR')
 
 petscurl = "https://bitbucket.org/petsc/petsc.git"
-slepcurl = "http://www.grycap.upv.es/slepc/svn/trunk"
+# slepcurl = "http://www.grycap.upv.es/slepc/svn/trunk"
+slepcurl = "http://www.grycap.upv.es/slepc/svn/branches/slepc-3_4-branch"
 
 petsc4pyurl = "https://code.google.com/p/petsc4py/"
-selpc4pyurl = "https://code.google.com/p/slepc4py/"
+slepc4pyurl = "https://code.google.com/p/slepc4py/"
 
 # syscallstdout =  os.open(os.devnull, os.O_RDWR)
 # syscallstderr = os.open(os.devnull, os.O_RDWR)
@@ -47,14 +46,15 @@ if tool == 'petsc':
 
     logger.info('cd ' + petscdir)
     os.chdir(petscdir)
-    #check_call(['git', 'pull'], stdout=syscallstdout, stderr=syscallstderr)
+    check_call(['git', 'pull'], stdout=syscallstdout, stderr=syscallstderr)
 
-
-    petsccompilecmd = ['./configure', os.getenv('PETSC_MAIN_FLAGS'), os.getenv('PETSC_OPT_FLAGS'), os.getenv('PETSC_DEBUGGING')]
+    petsccompilecmd = ['./configure', os.getenv('PETSC_MAIN_FLAGS'),
+                       os.getenv('PETSC_OPT_FLAGS'), os.getenv('PETSC_DEBUGGING')]
 
     for c in petsccompilecmd:
         print c
-    os.system(" ".join(petsccompilecmd))
+    check_call(petsccompilecmd, stdout=syscallstdout,
+               stderr=syscallstderr)
     os.system("make all")
 
 if tool == 'slepc':
@@ -67,7 +67,8 @@ if tool == 'slepc':
     logger.info('cd ' + slepcdir)
     os.chdir(slepcdir)
     check_call(['svn', 'update'], stdout=syscallstdout, stderr=syscallstderr)
-    os.system("./configure")
+    check_call(["./configure"], stdout=syscallstdout,
+               stderr=syscallstderr)
     logger.info("calling make")
     os.system("make all")
     logger.info("calling testscripts")
@@ -85,5 +86,18 @@ if tool == 'petsc4py':
     os.chdir(petsc4pydir)
     check_call(['hg', 'update'], stdout=syscallstdout, stderr=syscallstderr)
     check_call(['hg', 'pull'], stdout=syscallstdout, stderr=syscallstderr)
-    os.system("./configure")
-    os.system("make")
+    check_call(['make build'], stdout=syscallstdout, stderr=syscallstderr)
+
+if tool == 'slepc4py':
+    print "slepc4py dir", slepc4pydir
+    if not isdir(slepc4pydir):
+        cmd = ['hg', 'clone', slepc4pyurl, slepc4pydir]
+        logger.info("Checkout of Repo petsc4py with " + " ".join(cmd))
+        check_call(cmd, stdout=syscallstdout,
+                   stderr=syscallstderr)
+
+    logger.info('cd ' + slepc4pydir)
+    os.chdir(slepc4pydir)
+    check_call(['hg', 'update'], stdout=syscallstdout, stderr=syscallstderr)
+    check_call(['hg', 'pull'], stdout=syscallstdout, stderr=syscallstderr)
+    check_call(['make build'], stdout=syscallstdout, stderr=syscallstderr)
