1b1d1f6ec408e3eadce97a76c19c3aa9c4a88c87
Thomas Hisch <t.hisch@gmail.com>
2013-08-07 18:05:01 +0200

    [emacs] update python-shell-* vars

--- emacs.d/thi/python.conf.el
+++ emacs.d/thi/python.conf.el
@@ -1,11 +1,15 @@
 (setq
  python-shell-interpreter "ipython"
- python-shell-interpreter-args "--pylab"
+ python-shell-interpreter-args "--matplotlib"
  python-shell-prompt-regexp "In \\[[0-9]+\\]: "
  python-shell-prompt-output-regexp "Out\\[[0-9]+\\]: "
- python-shell-completion-setup-code ""
+ python-shell-completion-setup-code
+   "from IPython.core.completerlib import module_completion"
+ python-shell-completion-module-string-code
+   "';'.join(module_completion('''%s'''))\n"
  python-shell-completion-string-code
- "';'.join(get_ipython().Completer.all_completions('''%s'''))\n"
+   "';'.join(get_ipython().Completer.all_completions('''%s'''))\n"
+
  python-skeleton-autoinsert t )
 
 ;; (defvar ac-source-python
