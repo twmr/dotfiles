622feda682abe15a98943b9b5db91ec04adea8f1
Thomas Hisch <t.hisch@gmail.com>
2013-08-12 08:41:50 +0200

    [emacs] add unicode chars for calfw borders

--- emacs.d/thi/calfw.conf.el
+++ emacs.d/thi/calfw.conf.el
@@ -9,3 +9,14 @@
 
 ;; First day of the week
 (setq calendar-week-start-day 1) ; 0:Sunday, 1:Monday
+
+
+;; Unicode characters
+(setq cfw:fchar-junction ?╋
+      cfw:fchar-vertical-line ?┃
+      cfw:fchar-horizontal-line ?━
+      cfw:fchar-left-junction ?┣
+      cfw:fchar-right-junction ?┫
+      cfw:fchar-top-junction ?┯
+      cfw:fchar-top-left-corner ?┏
+      cfw:fchar-top-right-corner ?┓)
