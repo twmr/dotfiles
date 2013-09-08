1dcbb130ecb0c057bfea6bc144cc842088fdf5c2
Thomas Hisch <t.hisch@gmail.com>
2013-09-08 17:14:03 +0200

    slepcpetsc bin up

--- bin/petscslepc.py
+++ bin/petscslepc.py
@@ -3,7 +3,7 @@
 import logging
 import os
 from os.path import isdir
-from subprocess import check_call
+import subprocess
 
 petscdir = os.getenv('PETSC_DIR')
 petsc4pydir = os.getenv('PETSC4PY_DIR')
@@ -11,10 +11,10 @@ slepcdir = os.getenv('SLEPC_DIR')
 slepc4pydir = os.getenv('SLEPC4PY_DIR')
 
 petscurl = "https://bitbucket.org/petsc/petsc.git"
-# slepcurl = "http://www.grycap.upv.es/slepc/svn/trunk"
-slepcurl = "http://www.grycap.upv.es/slepc/svn/branches/slepc-3_4-branch"
+slepcurl = "http://www.grycap.upv.es/slepc/svn/trunk"
+# slepcurl = "http://www.grycap.upv.es/slepc/svn/branches/slepc-3_4-branch"
 
-petsc4pyurl = "https://code.google.com/p/petsc4py/"
+petsc4pyurl = "https://bitbucket.org/petsc/petsc4py"
 slepc4pyurl = "https://code.google.com/p/slepc4py/"
 
 # syscallstdout =  os.open(os.devnull, os.O_RDWR)
@@ -27,6 +27,10 @@ import sys
 if len(sys.argv) < 2:
     raise ValueError("specify a repo (in [slepc, petsc, "
                      "petscp4y, slepc4py]")
+
+def check_call(*args, **kwargs):
+    logger.info("executing %s" % (" ".join(args[0])))
+    return subprocess.check_call(*args, **kwargs)
 
 tool = sys.argv[1]
 
@@ -86,7 +90,7 @@ if tool == 'petsc4py':
     os.chdir(petsc4pydir)
     check_call(['hg', 'update'], stdout=syscallstdout, stderr=syscallstderr)
     check_call(['hg', 'pull'], stdout=syscallstdout, stderr=syscallstderr)
-    check_call(['make build'], stdout=syscallstdout, stderr=syscallstderr)
+    check_call(['make', 'build'], stdout=syscallstdout, stderr=syscallstderr)
 
 if tool == 'slepc4py':
     print "slepc4py dir", slepc4pydir
@@ -100,4 +104,4 @@ if tool == 'slepc4py':
     os.chdir(slepc4pydir)
     check_call(['hg', 'update'], stdout=syscallstdout, stderr=syscallstderr)
     check_call(['hg', 'pull'], stdout=syscallstdout, stderr=syscallstderr)
-    check_call(['make build'], stdout=syscallstdout, stderr=syscallstderr)
+    check_call(['make', 'build'], stdout=syscallstdout, stderr=syscallstderr)
