a9cb5e3f90818f94af5c6f21ade1d4faf82573e8
Thomas Hisch <t.hisch@gmail.com>
2013-09-29 22:46:40 +0200

    uncomment yas-prompt-functions

--- emacs.d/thi/yasnippet.conf.el
+++ emacs.d/thi/yasnippet.conf.el
@@ -1,16 +1,9 @@
 (setq yas-snippet-dirs '("~/.emacs.d/el-get/yasnippet/snippets" "~/.emacs.d/snippets"))
 (yas-global-mode 1)
 
-;;(defun yas-not-activate ()
-;;  (memq major-mode '(term-mode)))
-
-;;(set-default 'yas--dont-activate (cons #'yas-not-activate yas--dont-activate))
-
-;; (require 'dropdown-list)
-
-;; (setq yas/prompt-functions '(yas/ido-prompt
-;;                              yas/dropdown-prompt
-;;                              yas/completing-prompt))
+(setq yas/prompt-functions '(yas/ido-prompt
+                             yas/dropdown-prompt
+                             yas/completing-prompt))
 
 ;; Replace yasnippets's TAB
 ;; TODO study this
