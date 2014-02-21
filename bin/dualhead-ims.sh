228cb2b1f351f4340c35755746639c878568ee27
Thomas Hisch <thomas.hisch@ims.co.at>
2014-02-21 11:40:37 +0100

    dualhead setup up

--- bin/dualhead-ims.sh
+++ bin/dualhead-ims.sh
@@ -5,4 +5,4 @@ outright=DVI-I-2
 xrandr --output ${outleft} --auto
 xrandr --output ${outright} --auto
 xrandr --output ${outright} --right-of ${outleft}
-xrandr --output ${outright} --rotate left
+xrandr --output ${outright} --rotate normal
