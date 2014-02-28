5b8e8d299803ce1133583f91dd1379f3b260d342
Thomas Hisch <thomas.hisch@ims.co.at>
2014-02-28 09:20:13 +0100

    simpleimport up

--- bin/simpleimports.py
+++ bin/simpleimports.py
@@ -6,23 +6,10 @@ from poclib.core import ClientConnectorBase as Base
 from poclib.mathpack.misc import polar
 import quantities as pq
 
-psu = hw.PowerSupply('PSU_HVREF', verbose=True)
 
-ramper = hw.Ramper('RAMPER_PSYS_C', verbose=True)
+psu, mp2, cup, mpshutter, stage, session = map(
+    Base, ['PSU_HVREF', 'MP2', 'PAM_FC1', 'MP_SHUTTER', 'JEOL_STAGE',
+           'SessionManager'])
 
-mp2 = hw.Multipole('MP2', verbose=True)
-
-scanner = hw.Scanner('SCANNER1', verbose=True)
-scanner2 = hw.Scanner('SCANNER2', verbose=True)
-
-cup = hw.Cup('PAM_FC1',verbose=True)
-
-session = SessionManager('SessionManager', verbose=True)
-
-mp5 = Base('MP5', verbose=True)
-
-exp = Base('POC_Exposure', verbose=True)
-
-mpshutter = Base('MP_SHUTTER')
-
-print "ramper, cup, psu, scanner, scanner2, session, mp2 and mp5 objects initialized"
+print "ramper, cup, psu, scanner, scanner2, session, mp2 and mp5 objects " \
+    "initialized"
