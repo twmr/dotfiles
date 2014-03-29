c716c5c796d485b04a8e1bdd9081fcb71ce2b257
Thomas Hisch <thomas.hisch@ims.co.at>
2014-03-29 02:07:58 +0100

    [emacs] ansi term updates

--- emacs.d/lisp/thi/term.el
+++ emacs.d/lisp/thi/term.el
@@ -4,4 +4,20 @@
           (lambda() (progn
                       (setq yas-dont-activate t)
                       (setq global-hl-line-mode nil) ;; disable hl-line-mode
+                      (goto-address-mode) ;; clickable urls
                       )))
+
+;; force ansi-term to be utf-8 after it launches
+(defadvice ansi-term (after advise-ansi-term-coding-system activate)
+  (set-buffer-process-coding-system 'utf-8-unix 'utf-8-unix))
+
+;; http://echosa.github.io/blog/2012/06/06/improving-ansi-term/
+;; close the terminal buffer automatically on exit
+;; DOES NOT WORK ?!??!
+;; (defadvice term-sentinel (around my-advice-term-sentinel (proc msg))
+;;   (if (memq (process-status proc) '(signal exit))
+;;       (let ((buffer (process-buffer proc)))
+;;         ad-do-it
+;;         (kill-buffer buffer))
+;;     ad-do-it))
+;; (ad-activate 'term-sentinel)
