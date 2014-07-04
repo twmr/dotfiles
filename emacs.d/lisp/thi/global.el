5a9769565c733c904d517f0f9da22207747c1b7c
Thomas Hisch <t.hisch@gmail.com>
2014-07-04 10:08:44 +0200

    [emacs] more keybindings for re-builder

--- emacs.d/lisp/thi/global.el
+++ emacs.d/lisp/thi/global.el
@@ -264,7 +264,13 @@
       (pop-to-buffer reb-target-buffer)
       (call-interactively 'query-replace-regexp))))
 
-(define-key reb-mode-map (kbd "C-c %") 'my-reb-query-replace-regexp)
+(eval-after-load 're-builder
+	'(lambda ()
+     (define-key reb-mode-map (kbd "C-c %") 'my-reb-query-replace-regexp)
+     (define-key reb-mode-map (kbd "C-s") 'reb-next-match)
+     (define-key reb-mode-map (kbd "C-r") 'reb-prev-match)
+     ;; C-c C-q -> C-g
+     (define-key reb-mode-map (kbd "C-g") 'reb-copy-and-quit)))
 
 
 ;:::::::::::::::::::::::::::::::::::::::::::::::
