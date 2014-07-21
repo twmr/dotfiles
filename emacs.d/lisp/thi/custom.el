f46b3db2e676ee881f7e6b3f3220fcc5722762d7
Thomas Hisch <t.hisch@gmail.com>
2014-07-21 21:36:45 +0200

    [emacs] disable blink cursor mode globally
    (I don't like a blinking border in image-mode.)

--- emacs.d/lisp/thi/custom.el
+++ emacs.d/lisp/thi/custom.el
@@ -3,6 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(blink-cursor-mode nil)
  '(comint-input-ignoredups t)
  '(comint-prompt-read-only t)
  '(cursor-color nil)
