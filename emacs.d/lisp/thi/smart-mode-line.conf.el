8fab674b66ee5ba7572dec511bbe9e7c419ed899
Thomas Hisch <t.hisch@gmail.com>
2014-12-04 14:05:11 +0100

    [emacs] remove outdated projects from smart-mode-line config

--- emacs.d/lisp/thi/smart-mode-line.conf.el
+++ emacs.d/lisp/thi/smart-mode-line.conf.el
@@ -5,14 +5,8 @@
 (add-to-list 'sml/replacer-regexp-list '("^~/Dropbox/" ":DB:"))
 (add-to-list 'sml/replacer-regexp-list '("^~/gitrepos/" ":Git:"))
 
-(add-to-list 'sml/replacer-regexp-list '("^~/gitrepos/POC-scripts/branches/version2.0" ":psc:"))
-(add-to-list 'sml/replacer-regexp-list '("^~/gitrepos/tools/alpha" ":A:"))
-(add-to-list 'sml/replacer-regexp-list '("^~/gitrepos/tools/beta1" ":B1:"))
-(add-to-list 'sml/replacer-regexp-list '("^~/gitrepos/tools/beta2" ":B2:"))
-(add-to-list 'sml/replacer-regexp-list '("^~/gitrepos/tools/future" ":F:"))
-
-(add-to-list 'sml/replacer-regexp-list '("^~/gitrepos/HWSimuEnv" ":HWSIM:"))
-(add-to-list 'sml/replacer-regexp-list '("^~/gitrepos/imshelperscripts" ":IH:"))
+(add-to-list 'sml/replacer-regexp-list '("^~/gitrepos/netgen/netgen" ":ng:"))
+(add-to-list 'sml/replacer-regexp-list '("^~/gitrepos/ngsolve/ngsolve" ":ns:"))
 
 (add-to-list 'sml/replacer-regexp-list '("^~/gitrepos/dotfiles/emacs.d" ":ED:"))
 (add-to-list 'sml/replacer-regexp-list '("^~/.emacs.d" ":ED:"))
