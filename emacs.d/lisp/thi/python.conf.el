18389eadc3f7b2d41d8a6ab271b023c7409ab2bf
Thomas Hisch <t.hisch@gmail.com>
2014-12-12 09:30:11 +0100

    [emacs] set python-interpreter to ipython
    completion logic is in python.el and no longer needed in my
    python.conf.el

--- emacs.d/lisp/thi/python.conf.el
+++ emacs.d/lisp/thi/python.conf.el
@@ -1,13 +1,7 @@
 ;; ;; the following code does not work when we run emacs from a remove pc (ssh)
-;; (setq python-shell-interpreter "ipython"
-;;       python-shell-interpreter-args "--matplotlib"
-;;       python-shell-completion-setup-code
-;;       "from IPython.core.completerlib import module_completion"
-;;       python-shell-completion-module-string-code
-;;       "';'.join(module_completion('''%s'''))\n"
-;;       python-shell-completion-string-code
-;;       "';'.join(get_ipython().Completer.all_completions('''%s'''))\n"
-;; )
+
+(setq python-shell-interpreter "ipython"
+      python-shell-interpreter-args "-i")
 
 (setq python-skeleton-autoinsert t)
 
