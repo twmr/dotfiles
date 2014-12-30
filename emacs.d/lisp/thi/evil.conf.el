e411e3c90ace21bf3b0650928bccc43ca954bb4e
Thomas Hisch <t.hisch@gmail.com>
2014-12-30 10:05:12 +0100

    [emacs] bind C-e to end-of-line in evil states

--- emacs.d/lisp/thi/evil.conf.el
+++ emacs.d/lisp/thi/evil.conf.el
@@ -48,6 +48,8 @@
 (define-key evil-normal-state-map "gs" 'magit-status)
 (define-key evil-insert-state-map "\C-k" 'kill-line)
 (define-key evil-normal-state-map "\C-k" 'kill-line)
+(define-key evil-insert-state-map "\C-e" 'end-of-line)
+(define-key evil-normal-state-map "\C-e" 'end-of-line)
 
 (define-key evil-insert-state-map [remap newline] 'evil-ret-and-indent)
 
