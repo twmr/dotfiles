b29fec560dc04eb2b30acd1d94a56aba54a2b395
Thomas Hisch <thomas.hisch@tuwien.ac.at>
2015-06-01 11:56:51 +0200

    new monitor setup @ tu
    Signed-off-by: Thomas Hisch <thomas.hisch@tuwien.ac.at>

--- bin/dualhead-tu.sh
+++ bin/dualhead-tu.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-outleft=HDMI2
-outright=VGA1
+outleft=VGA1
+outright=HDMI2
 xrandr --output ${outleft} --auto
 xrandr --output ${outright} --auto
 xrandr --output ${outright} --right-of ${outleft}
