02876973f3bd27ec1c4d13c20d989167406cadf2
Thomas Hisch <thomas.hisch@tuwien.ac.at>
2015-11-28 22:22:11 +0100

    [emacs] company stuff

--- emacs.d/lisp/thi/company-mode.conf.el
+++ emacs.d/lisp/thi/company-mode.conf.el
@@ -1,1 +1,19 @@
-(define-key company-active-map (kbd "<tab>") 'company-complete)
+;; (define-key company-active-map (kbd "<tab>") 'company-complete)
+
+(defun company-eshell-history (command &optional arg &rest ignored)
+  (interactive (list 'interactive))
+  (cl-case command
+    (interactive (company-begin-backend 'company-eshell-history))
+    (prefix (and (eq major-mode 'eshell-mode)
+                 (let ((word (company-grab-word)))
+                   (save-excursion
+                     (eshell-bol)
+                     (and (looking-at-p (s-concat word "$")) word)))))
+    (candidates (remove-duplicates
+                 (->> (ring-elements eshell-history-ring)
+                      (remove-if-not (lambda (item) (s-prefix-p arg item)))
+                      (mapcar 's-trim))
+                 :test 'string=))
+    (sorted t)))
+
+(add-to-list 'company-backends 'company-eshell-history)
