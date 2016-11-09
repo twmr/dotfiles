14ba99740a199d636bc86e950efb6d8a78335dd1
Thomas Hisch <t.hisch@gmail.com>
2016-11-09 21:00:58 +0100

    [emacs] reformatted code in defuns.el

--- emacs.d/lisp/thi/defuns.el
+++ emacs.d/lisp/thi/defuns.el
@@ -11,17 +11,16 @@
   "Print number of words and chars in region."
   (interactive "r")
   (message "Counting \u2026")
-  (save-excursion
-    (let (wordCount charCount)
-      (setq wordCount 0)
-      (setq charCount (- posEnd posBegin))
-      (goto-char posBegin)
-      (while (and (< (point) posEnd)
-                  (re-search-forward "\\w+\\W*" posEnd t))
-        (setq wordCount (1+ wordCount)))
+  (save-excursion (let (wordCount charCount)
+                    (setq wordCount 0)
+                    (setq charCount (- posEnd posBegin))
+                    (goto-char posBegin)
+                    (while (and (< (point) posEnd)
+                                (re-search-forward "\\w+\\W*" posEnd t))
+                      (setq wordCount (1+ wordCount)))
 
-      (message "Words: %d. Chars: %d." wordCount charCount)
-      )))
+                    (message "Words: %d. Chars: %d." wordCount charCount)
+                    )))
 
 (defun visit-term-buffer ()
   "Create or visit a terminal buffer."
@@ -35,25 +34,25 @@
 
 
 (defun open-file-at-cursor ()
-    "Open the file path under cursor.
+  "Open the file path under cursor.
 If there is text selection, uses the text selection for path.
 If the path is starts with “http://”, open the URL in browser.
 Input path can be {relative, full path, URL}.
 This command is similar to `find-file-at-point' but without prompting for confirmation.
 "
-    (interactive)
-    (let ( (path (if (region-active-p)
-                     (buffer-substring-no-properties (region-beginning) (region-end))
-                   (thing-at-point 'filename) ) ))
-      (if (string-match-p "\\`https?://" path)
-          (browse-url path)
-        (progn ; not starting “http://”
-          (if (file-exists-p path)
-              (find-file path)
-            (if (file-exists-p (concat path ".el"))
-                (find-file (concat path ".el"))
-              (when (y-or-n-p (format "file doesn't exist: 「%s」. Create?" path) )
-                (find-file path )) ) ) ) ) ))
+  (interactive)
+  (let ( (path (if (region-active-p)
+                   (buffer-substring-no-properties (region-beginning) (region-end))
+                 (thing-at-point 'filename) ) ))
+    (if (string-match-p "\\`https?://" path)
+        (browse-url path)
+      (progn ; not starting “http://”
+        (if (file-exists-p path)
+            (find-file path)
+          (if (file-exists-p (concat path ".el"))
+              (find-file (concat path ".el"))
+            (when (y-or-n-p (format "file doesn't exist: 「%s」. Create?" path) )
+              (find-file path )) ) ) ) ) ))
 
 ;; from https://gist.github.com/sabof/5383987
 (defun wc/narrow-window ()
