37842095d607bbda17acdf5f286cefe67fb43888
Thomas Hisch <t.hisch@gmail.com>
2016-11-21 23:34:10 +0100

    ipython config file
    Signed-off-by: Thomas Hisch <t.hisch@gmail.com>

---
+++ ipython/profile_default/ipython_config.py
@@ -0,0 +1,6 @@
+c = get_config()
+c.TerminalIPythonApp.display_banner = False
+c.TerminalIPythonApp.profile = u'default'
+c.TerminalInteractiveShell.history_length = 10000
+c.TerminalInteractiveShell.autoindent = True
+c.TerminalInteractiveShell.confirm_exit = False
