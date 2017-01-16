4cbd7c6f164bfae307ba4a8b2f0921810160ceba
Thomas Hisch <t.hisch@gmail.com>
2017-01-16 22:38:49 +0100

    update start-gnome-session.sh script

--- xsessions/start-gnome-session.sh
+++ xsessions/start-gnome-session.sh
@@ -4,13 +4,12 @@
 
 # if something with the gnome-session does go wrong look at .xsession-errors in your homedir
 
-MYWM=$1
-SHOW_DESKTOP=$2
-GNOME3=true
 HOSTNAME=`hostname`
 
 SESSKEY="/desktop/gnome/session"
 GSETTOOL="/usr/bin/gconftool-2"
+
+export PATH=$HOME/.bin:$PATH
 
 # Load resources
 if [ -f $HOME/.Xresourses ]; then
@@ -23,25 +22,30 @@ fi
 
 # Set the background color
 #/usr/bin/xsetroot -solid black
-pgrep -x -u $USER urxvtd > /dev/null || urxvtd -o -f &
+#pgrep -x -u $USER urxvtd > /dev/null || urxvtd -o -f &
 
 #The following line is now in gnome-session
-pgrep -u $USER emacs > /dev/null || emacs --daemon &
+# pgrep -u $USER emacs > /dev/null || emacs --daemon &
 
 #international keyboard layout
-setxkbmap us altgr-intl
+#setxkbmap us altgr-intl
+if [ "$HOSTNAME" = "cobra" ]; then
+    keyboard.sh
+    laptop-settings.sh
+fi
+
 # xmodmap ~/.Xmodmap
 
-setxkbmap -option ctrl:swapcaps     # Swap Left Control and Caps Lock
+# setxkbmap -option ctrl:swapcaps     # Swap Left Control and Caps Lock
 # setxkbmap -option ctrl:nocaps       # Make Caps Lock a Control key
 
-~/gitrepos/dotfiles/bin/dualhead-ims.sh
+#~/gitrepos/dotfiles/bin/dualhead-ims.sh
 
 /usr/bin/gnome-keyring-daemon --start --components=pkcs11
 /usr/bin/gnome-keyring-daemon --start --components=ssh
 /usr/bin/gnome-keyring-daemon --start --components=gpg
 
-$GSETTOOL -u $SESSKEY/required_components/windowmanager
+#$GSETTOOL -u $SESSKEY/required_components/windowmanager
 
 #if something does not work as expected start gnome-session with --debug and look into .xsession-errors
 #-a starts only the *.desktop files in the specified dir instead of the WM  (ex: -a /home/thomas/dumm)
@@ -56,7 +60,10 @@ $GSETTOOL -u $SESSKEY/required_components/windowmanager
 #   export SSH_AUTH_SOCK
 #fi
 
-exec ssh-agent /usr/bin/awesome
+nm-applet &
 
-#rm -f ~/.config/gnome-session/saved-session/gnome-shell.desktop
+exec ssh-agent `which awesome`
+
+# rm -f $HOME/.config/gnome-session/saved-session/gnome-shell.desktop
 #exec gnome-session --debug --session=awesome
+exec gnome-session
