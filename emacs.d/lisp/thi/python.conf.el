3be23d91bc1bee103d816537343fd943b61ae4a9
Thomas Hisch <t.hisch@gmail.com>
2017-04-18 21:11:10 +0200

    [emacs] set python interpreter back to normal 'python'

--- emacs.d/lisp/thi/python.conf.el
+++ emacs.d/lisp/thi/python.conf.el
@@ -2,9 +2,9 @@
 
 ;; (setq python-shell-interpreter "ipython"
 ;;       python-shell-interpreter-args "-i")
-(setq python-shell-interpreter "ipython"
-      python-shell-interpreter-args "--simple-prompt")
-
+;; (setq python-shell-interpreter "ipython"
+;;       python-shell-interpreter-args "--simple-prompt")
+(setq python-shell-interpreter "python")
 (setq python-skeleton-autoinsert t)
 
 ;; (defvar ac-source-python
@@ -43,33 +43,33 @@
 ;; (add-hook 'python-mode-hook #'turn-on-auto-fill 1)
 ;; (add-hook 'python-mode-hook (lambda ()
 ;;                               (push '("lambda" . 955) prettify-symbols-alist)))
-(add-hook
- 'python-mode-hook
- (lambda ()
-   (mapc (lambda (pair) (push pair prettify-symbols-alist))
-         '(;; Syntax
-           ("def" .      #x2131)
-           ("not" .      #x2757)
-           ("in" .       #x2208)
-           ("not in" .   #x2209)
-           ("return" .   #x27fc)
-           ("yield" .    #x27fb)
-           ("for" .      #x2200)
-           ("lambda" .      955)
-           ;; Base Types
-           ("int" .      #x2124)
-           ("float" .    #x211d)
-           ("str" .      #x1d54a)
-           ("True" .     #x1d54b)
-           ("False" .    #x1d53d)
-           ;; Mypy
-           ("Dict" .     #x1d507)
-           ("List" .     #x2112)
-           ("Tuple" .    #x2a02)
-           ("Set" .      #x2126)
-           ("Iterable" . #x1d50a)
-           ("Any" .      #x2754)
-           ("Union" .    #x22c3)))))
+;; (add-hook
+;;  'python-mode-hook
+;;  (lambda ()
+;;    (mapc (lambda (pair) (push pair prettify-symbols-alist))
+;;          '(;; Syntax
+;;            ("def" .      #x2131)
+;;            ("not" .      #x2757)
+;;            ("in" .       #x2208)
+;;            ("not in" .   #x2209)
+;;            ("return" .   #x27fc)
+;;            ("yield" .    #x27fb)
+;;            ("for" .      #x2200)
+;;            ("lambda" .      955)
+;;            ;; Base Types
+;;            ("int" .      #x2124)
+;;            ("float" .    #x211d)
+;;            ("str" .      #x1d54a)
+;;            ("True" .     #x1d54b)
+;;            ("False" .    #x1d53d)
+;;            ;; Mypy
+;;            ("Dict" .     #x1d507)
+;;            ("List" .     #x2112)
+;;            ("Tuple" .    #x2a02)
+;;            ("Set" .      #x2126)
+;;            ("Iterable" . #x1d50a)
+;;            ("Any" .      #x2754)
+;;            ("Union" .    #x22c3)))))
 
 
 ;; redefine jedi's C-. (jedi:goto-definition)
