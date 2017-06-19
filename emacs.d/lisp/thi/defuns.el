e6f59b34e24790e22987d1a2396bc305b538048a
Thomas Hisch <thomas.hisch@ims.co.at>
2017-06-19 10:39:55 +0200

    [emacs] sudo-edit

--- emacs.d/lisp/thi/defuns.el
+++ emacs.d/lisp/thi/defuns.el
@@ -338,3 +338,15 @@ Position the cursor at its beginning, according to the current mode."
   ;; TODO #lines that can be output using "message" are limited
   (message "PYTHONPATH\n%s" (mapconcat #'identity (split-string
                                                    (getenv "PYTHONPATH") ":") "\n")))
+
+(defun sudo-edit (&optional arg)
+  "Edit currently visited file as root.
+
+With a prefix ARG prompt for a file to visit.
+Will also prompt for a file to visit if current
+buffer is not visiting a file."
+  (interactive "P")
+  (if (or arg (not buffer-file-name))
+      (find-file (concat "/sudo:root@localhost:"
+                         (ido-read-file-name "Find file(as root): ")))
+    (find-alternate-file (concat "/sudo:root@localhost:" buffer-file-name))))
