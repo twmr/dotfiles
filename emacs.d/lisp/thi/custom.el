537ec022d5bcfb6407b34a146d7f68b50314b2f4
Thomas Hisch <thomas.hisch@ims.co.at>
2018-03-26 05:35:53 +0200

    [emacs] git-commit msg confgi

--- emacs.d/lisp/thi/custom.el
+++ emacs.d/lisp/thi/custom.el
@@ -21,6 +21,7 @@
  '(git-commit-known-pseudo-headers
    '("Signed-off-by" "Acked-by" "Modified-by" "Cc" "Suggested-by" "Reported-by" "Tested-by" "Reviewed-by" "Fixes" "Related" "Change-Id"))
  '(git-commit-mode-hook '(turn-on-auto-fill) t)
+ '(git-commit-summary-max-length 50)
  '(global-eldoc-mode nil)
  '(grep-find-ignored-directories
    '("SCCS" "RCS" "CVS" "MCVS" ".src" ".svn" ".git" ".hg" ".bzr" "_MTN" "_darcs" "{arch}" "build"))
