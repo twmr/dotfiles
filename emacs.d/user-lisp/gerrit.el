b60024a4624cd69c7e9e1a032e7d1eb50289d7ba
Thomas Hisch <t.hisch@gmail.com>
2019-03-13 22:08:01 +0100

    [emacs] wip magit-section stuff

--- emacs.d/user-lisp/gerrit.el
+++ emacs.d/user-lisp/gerrit.el
@@ -171,28 +171,135 @@ gerrit-upload: (current cmd: %(concat (gerrit-upload-create-git-review-cmd)))
       (magit-git-command (concat "git review -d "
                                  (car (s-split " " (s-trim changenr))))))))
 
+(defun foobar ()
+  (interactive)
+  (message "enterpressed"))
+
+(defun foobar2 ()
+  (interactive)
+  (message "enterpressed (2)"))
+
+;; only for heading
+(defvar magit-open-reviews-section-map
+  (let ((map (make-sparse-keymap)))
+    (define-key map "jT" #'magit-todos-jump-to-todos)
+    (define-key map "jl" #'magit-todos-list)
+    (define-key map (kbd "RET") 'foobar)
+    map)
+  "Keymap for `magit-open-reviews' top-level section.")
+
+;; for issues
+(defvar magit-open-reviews-issue-section-map
+  (let ((map (make-sparse-keymap)))
+    (define-key map "jT" #'magit-todos-jump-to-todos)
+    (define-key map "jl" #'magit-todos-list)
+    (define-key map (kbd "RET") 'foobar2)
+    map)
+  "Keymap for `magit-open-reviews' top-level section.")
+
 (defun magit-gerrit-insert-status ()
-  (let* ((name "hello world"))
-    (magit-insert-section (gerrit)
-      ;; TODO tabulated-list-init-header
-      (magit-insert-heading "Open Gerrit Reviews")
-      (magit-insert-section (gerrit-info)
-        (insert (format "Name:   %s\n" name))
-        (insert (format "Name0:   %s\n" name))
-        (magit-insert-heading)
-        (insert (format "Name2:   %s\n" name))
+  (magit-insert-section (open-reviews)
+    ;; the behavoir should be similar to "Recent commits"
+    (magit-insert-heading "Open Gerrit Reviews")
+    (dolist (loopvar `(1245 1249 1222 1234 0091))
+      ;; (magit-insert-heading
+      (progn
+        (magit-insert-section (open-reviews-issue loopvar t)
+          (magit-insert-heading
+            (format (format "%%%ds (%%s) %%s\n" (1+ 4)) ;1+ accounts for #
+                    (propertize (format "#%d" loopvar))
+                    (propertize "version7.0" 'face '(:foreground "red"))
+                    (propertize "foobar")))
+          (insert "metadata1\n"))
         ;; (magit-insert-heading)
-        (insert (format "Name3:   %s\n" name)))
-      (magit-insert-section (gerrit-dummy)
-        (magit-insert-heading
-          (propertize "Diagram\n"
-                      'face 'magit-section-secondary-heading))
+        ;; (insert "Metadata1: xxx\n")
+        ;; (insert "Metadata2: yyy\n")
+        ))
+    (insert ?\n)
+    ))
+
+
+(defun magit-gerrit-insert-status2 ()
+  "Insert information about current incremental merge."
+  (when (magit-imerge-in-progress-p)
+    (let* ((name (or (magit-imerge-current-name)
+                     (error "No name, but in progress?")))
+           (state (magit-imerge-state name))
+           (format-with-overriding
+            (lambda (option current)
+              (let ((val (--some
+                          (and (string-match (format "\\`%s=\\(.+\\)"
+                                                     (regexp-quote option))
+                                             it)
+                               (match-string 1 it))
+                          magit-imerge--arguments)))
+                (if (and val (not (string= val current)))
+                    (propertize val 'face 'magit-imerge-overriding-value)
+                  current)))))
+      (magit-insert-section (imerge)
+        (magit-insert-heading "Incremental merge")
+        (magit-insert-section (imerge-info)
+          (insert (format "Name:   %s\n" name))
+          (magit-insert-heading)
+          (insert (format "Goal:   %s\n"
+                          (funcall format-with-overriding
+                                   "--goal"
+                                   (cdr (assq 'goal state)))))
+          (insert (format "Result: %s\n"
+                          (funcall format-with-overriding
+                                   "--branch"
+                                   (cdr (assq 'branch state)))))
+          (insert "Tips:   ")
+          (magit-imerge--insert-tip (cdr (assq 'tip1 state)))
+          (insert ", ")
+          (magit-imerge--insert-tip (cdr (assq 'tip2 state)))
+          (insert ?\n ?\n))
+        (magit-insert-section (imerge-diagram)
+          (magit-insert-heading
+            (propertize "Diagram\n"
+                        'face 'magit-section-secondary-heading))
           (insert
            (with-temp-buffer
-             (magit-git-insert "status")
-           ;;   (re-search-backward "^Untracked")
-           ;;   (delete-region (point) (point-max))
-             (buffer-string)))))))
+             (magit-git-insert "imerge" "diagram" "--no-color" "--commits")
+             (re-search-backward "^Key:")
+             (delete-region (point) (point-max))
+             (buffer-string))))))))
+
+
+
+(defun magit-gerrit-insert-status3 (&optional type value)
+  "Insert section showing recent commits.
+Show the last `magit-log-section-commit-count' commits."
+  (let* ((start (format "HEAD~%s" magit-log-section-commit-count))
+         (range (and (magit-rev-verify start)
+                     (concat start "..HEAD"))))
+
+    (magit-insert-section (open-reviews)
+      (magit-insert-heading "Open Gerrit Reviews")
+      (magit-insert-section (longer)
+        (insert-text-button
+         (substitute-command-keys
+          (format "Type \\<%s>\\[%s] to show more history"
+                  'magit-log-mode-map
+                  'magit-log-double-commit-limit))
+         'action (lambda (_button)
+                   (message "clicked"))
+         ;; (magit-log-double-commit-limit))
+         'follow-link t
+         'mouse-face 'magit-section-highlight))
+
+      (insert ?\n)
+      (insert (format "l1\nl2 %s\n" range))
+
+      (magit-insert-log range
+                        (cons (format "-n%d" magit-log-section-commit-count)
+                              (--remove (string-prefix-p "-n" it)
+                                        magit-log-section-arguments))))))
+
+
+
+;; (add-hook 'magit-status-sections-hook #'magit-gerrit-insert-status t)
+
 
 (add-hook 'magit-status-sections-hook #'magit-gerrit-insert-status t)
 (provide 'gerrit)
