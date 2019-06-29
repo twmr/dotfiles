d87a03f41c5443494a373d20a16d3101dcd1d1aa
Thomas Hisch <t.hisch@gmail.com>
2019-06-29 21:13:40 +0200

    Remove unused scripts

--- bin/dualhead-tu.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-outleft=HDMI1
-outright=HDMI2
-xrandr --output ${outleft} --auto
-xrandr --output ${outright} --auto
-xrandr --output ${outright} --right-of ${outleft}
-xrandr --output ${outright} --rotate normal
