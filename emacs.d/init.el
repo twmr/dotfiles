8462275ddba1cccc93c10acac3cce90300ccb64b
Thomas Hisch <t.hisch@gmail.com>
2019-12-05 21:30:13 +0100

    [emacs] remove unsused luamode

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -865,8 +865,6 @@ See URL `https://www.pylint.org/'."
 
 (use-package json-mode :ensure t :defer t)
 
-(use-package lua-mode :ensure t :defer t)
-
 (use-package magit
   ;; bindings: C-c M-g: magit-file-popup (use it in a buffer)
   :ensure t
