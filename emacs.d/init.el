6f7cd373678d3c29fead70239c1af3bf2133bd48
Thomas Hisch <t.hisch@gmail.com>
2019-12-11 21:41:38 +0100

    [emacs] add equake

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -365,6 +365,14 @@
 ;;   :init
 ;;   (elpy-enable))
 
+(use-package equake
+    :ensure t
+    :config  ; some examples of optional settings follow:
+    (global-set-key (kbd "C-x C-c") 'equake-check-if-in-equake-frame-before-closing) ; prevent accidental frame-closure
+    (setq equake-size-width 0.99) ; set width a bit less than full-screen (prevent 'overflow' on multi-monitor)
+    ;; set distinct face for Equake: white foreground with dark blue background, and different font
+    (set-face-attribute 'equake-buffer-face 'nil :inherit 'default :family "DejaVu Sans Mono" :background "#000022" :foreground "white"))
+
 (use-package evil :ensure t
   :config (progn
           ;; (delete 'term-mode evil-insert-state-modes)
