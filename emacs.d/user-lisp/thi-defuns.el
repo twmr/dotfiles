903a5a995a6f0c9de4dbccc5774e91ffe7702dc6
Thomas Hisch <t.hisch@gmail.com>
2019-12-11 22:43:06 +0100

    [emacs] add git-blame-line defun

--- emacs.d/user-lisp/thi-defuns.el
+++ emacs.d/user-lisp/thi-defuns.el
@@ -404,3 +404,27 @@ buffer is not visiting a file."
        (transpose-lines -1))
      (forward-line -1)
      (move-to-column col)))
+
+
+(defun git-blame-line ()
+  "Runs `git blame` on the current line and
+   adds the commit id to the kill ring"
+  ;; from https://www.jayconrod.com/posts/67/emacs-run-gitblame-on-the-current-line
+  (interactive)
+  (let* ((line-number (save-excursion
+                        (goto-char (point-at-bol))
+                        (+ 1 (count-lines 1 (point)))))
+         (line-arg (format "%d,%d" line-number line-number))
+         (commit-buf (generate-new-buffer "*git-blame-line-commit*")))
+    (call-process "git" nil commit-buf nil
+                  "blame" (buffer-file-name) "-L" line-arg)
+    (let* ((commit-id (with-current-buffer commit-buf
+                        (buffer-substring 1 9)))
+           (log-buf (generate-new-buffer "*git-blame-line-log*")))
+      (kill-new commit-id)
+      (call-process "git" nil log-buf nil
+                    "log" "-1" "--pretty=%h   %an   %s" commit-id)
+      (with-current-buffer log-buf
+        (message "Line %d: %s" line-number (buffer-string)))
+      (kill-buffer log-buf))
+    (kill-buffer commit-buf)))
