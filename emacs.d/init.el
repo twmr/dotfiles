be70495d72c29c799622254fa68218d1199204ad
Thomas Hisch <t.hisch@gmail.com>
2020-08-14 22:20:46 +0200

    [emacs] Move thi-progrmodes into init.el

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -1510,6 +1510,46 @@ See URL `https://www.pylint.org/'."
 
 (use-package pip-requirements :ensure t :defer t)
 
+(use-package prog-mode
+  :config
+  (setqq thi::programming-language-major-modes
+    '(prog-mode     ; This is the mode perl, makefile, lisp-mode, scheme-mode,
+                                        ; emacs-lisp-mode, sh-mode, java-mode, c-mode, c++-mode,
+                                        ; python-mode inherits from.
+      lua-mode
+      cmake-mode
+      tex-mode      ; LaTeX inherits
+      sgml-mode     ; HTML inherits
+      css-mode
+      nxml-mode
+      diff-mode
+      haskell-mode
+      rst-mode))
+
+  (defun thi::customize-programming-language-mode ()
+    (font-lock-add-keywords
+     nil
+     '(("\\<\\(FIXME\\|HACK\\|XXX\\|TODO\\|NOTE\\|REFACTOR\\|FIX\\)"
+        1
+        '(:box (:color "grey10" :line-width 2) :background "red" :foreground "yellow")
+        prepend)))
+    (setq show-trailing-whitespace t)
+    (eldoc-mode -1)
+    ;; (rainbow-delimiters-mode 1)
+    ;; (idle-highlight-mode 1)
+    ;; temporarily disabled the rainbow modes as i think they cause speed problems
+    ;; (rainbow-mode 1)
+    ;; (flyspell-prog-mode)
+    )
+
+  (dolist (mode thi::programming-language-major-modes)
+    (add-hook
+     (intern (concat (symbol-name mode) "-hook"))
+     #'thi::customize-programming-language-mode))
+
+  (add-hook 'prog-mode-hook #'outline-minor-mode)
+  (add-hook 'prog-mode-hook #'hs-minor-mode))
+
 (use-package projectile :ensure t
   :custom (projectile-completion-system 'ivy)
   :bind (("C-x f" . projectile-find-file))
@@ -1937,7 +1977,6 @@ See URL `https://www.pylint.org/'."
 
 (load "thi-defuns")
 (load "thi-global")
-(load "thi-progmodes")
 (load "thi-projects")
 (load "thi-danjou")
 (load "thi-recentf")
