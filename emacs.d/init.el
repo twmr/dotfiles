2d80cf36f0a550549ca14807ea95130ad7221322
Thomas Hisch <t.hisch@gmail.com>
2020-12-06 10:19:59 +0100

    [emacs] Disable spell-fu-mode in magit-section-mode buffers

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -1826,7 +1826,7 @@ See URL `https://www.pylint.org/'."
   (define-global-minor-mode thi::spell-fu-mode spell-fu-mode
     (lambda ()
       (when (not (memq major-mode
-                       (list 'vterm-mode 'gerrit-dashboard-mode 'magit-status-mode 'dired-mode)))
+                       (list 'vterm-mode 'gerrit-dashboard-mode 'magit-status-mode 'magit-section-mode 'dired-mode)))
         (spell-fu-mode))))
 
   (thi::spell-fu-mode 1)
