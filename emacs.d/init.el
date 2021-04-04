32b0fea8d6c23493e13f3b0855d9f15a7282574f
Thomas Hisch <thomas.hisch@ims.co.at>
2021-04-04 21:14:51 +0200

    Move org configs to init.el
    Change-Id: I66a882a57ced3f2d0bc47a2f6f3d3ff6fef09493

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -1527,6 +1527,14 @@ shown in the section buffer."
 
   :ensure t
   :custom
+  ;; this will turn off asking for a confirmation
+  ;; (org-agenda-files (quote ("~/gitrepos/orgnotes/studium.org"
+  ;;                           "~/gitrepos/orgnotes/laserPA.org"
+  ;;                           "~/gitrepos/orgnotes/diplomarbeit.org"
+  ;;                           "~/gitrepos/orgnotes/physik.org"
+  ;;                           "~/gitrepos/orgnotes/ims.org"
+  ;;                           "~/gitrepos/orgnotes/emacs.org")))
+  (org-confirm-babel-evaluate nil)
   (org-directory "~/org")
   (org-default-notes-file (concat org-directory "/gtd.org"))
   (org-capture-templates
@@ -1541,12 +1549,132 @@ shown in the section buffer."
       (file+olp+datetree (concat org-directory "journal.org")
       (file "~/.emacs.d/org-templates/journal.oRgcaptmpl")))
    ))
-  :bind (("C-c c" . org-capture))
+  :bind (("C-c c" . org-capture)
+         ("C-c l" . org-store-link)
+         ("C-c a" . org-agenda)
+         ;; ("C-c b" . org-iswitchb)
+         :map org-mode-map
+         ("C-`" . helm-semantic-or-imenu)
+         )
   :config
-  (org-babel-do-load-languages
-   'org-babel-load-languages
-   '((emacs-lisp . nil)
-     (python . t))))
+
+  (add-hook 'org-babel-after-execute-hook 'org-display-inline-images)
+
+
+  ;; fontify src blocks, doesn't work with fixed-pitch blocks :(
+  ;;(setq org-src-fontify-natively t)
+
+  ;; (setq org-hide-leading-stars t)
+  ;; (setq org-use-speed-commands t)
+  ;; (setq org-speed-commands-user (quote (("0" . delete-window)
+  ;;                                       ("1" . delete-other-windows)
+  ;;                                       ("2" . split-window-vertically)
+  ;;                                       ("3" . split-window-horizontally)
+  ;;                                       ("h" . hide-other)
+  ;;                                       ("k" . org-kill-note-or-show-branches)
+  ;;                                       ("r" . org-reveal)
+  ;;                                       ("s" . org-save-all-org-buffers)
+  ;;                                       ("z" . org-add-note)
+  ;;                                       ("c" . self-insert-command)
+  ;;                                       ("C" . self-insert-command)
+  ;;                                       ("J" . org-clock-goto))))
+  ;;
+
+  ;; taken from http://tincman.wordpress.com/2011/01/04/research-paper-management-with-emacs-org-mode-and-reftex/
+  ;;
+
+  ;; needed because every time when i load a org file a get asked for a master file name ?!?!!
+  ;; (setq reftex-default-bibliography
+  ;;       '("~/gitrepos/orgnotes/refs.bib"))
+
+  ;; (setq org-link-abbrev-alist
+  ;;       '(("bib" . "~/gitrepos/orgnotes/refs.bib::%s")
+  ;;         ("diplomarbeit" . "~/gitrepos/orgnotes/diplomarbeit.org::#%s")
+  ;;         ("papers" . "~/Dropbox/research/%s.pdf")))
+
+  ;; (defun org-mode-reftex-search ()
+  ;;   ;;jump to the notes for the paper pointed to at from reftex search
+  ;;   (interactive)
+  ;;   (org-open-link-from-string (format "[[diplomarbeit:%s]]" (reftex-citation t))))
+
+  ;; (defun org-mode-reftex-setup ()
+  ;;   (load-library "reftex")
+  ;;   (and (buffer-file-name) (file-exists-p (buffer-file-name))
+  ;;        (progn
+  ;;          ;enable auto-revert-mode to update reftex when bibtex file changes on disk
+  ;;          (global-auto-revert-mode t)
+
+  ;;          ;; this prevent reftex asking for the master file
+  ;;          (make-local-variable 'TeX-master)
+  ;;          (setq TeX-master t)
+
+  ;;          (reftex-parse-all)
+
+  ;;          ;add a custom reftex cite format to insert links
+  ;;          (reftex-set-cite-format
+  ;;           '((?b . "[[bib:%l][%l-bib]]")
+  ;;             (?n . "[[diplomarbeit:%l][%l-diplomarbeit]]")
+  ;;             (?p . "[[papers:%l][%l-paper]]")
+  ;;             (?c . "\\cite{%l}")
+  ;;             (?t . "%t")
+  ;;             (?h . "\n** [[papers:%l][%t]]\n  :PROPERTIES:\n  :Custom_ID: %l\n  :END:\n")))))
+
+  ;;   (define-key org-mode-map (kbd "C-c )") 'reftex-citation)
+  ;;   (define-key org-mode-map (kbd "C-c (") 'org-mode-reftex-search))
+
+  ;; (add-hook 'org-mode-hook 'org-mode-reftex-setup)
+  ;; (add-hook 'org-mode-hook 'turn-on-visual-line-mode)
+
+  ;;beamer
+
+  ;; allow for export=>beamer by placing
+
+  ;; #+LaTeX_CLASS: beamer in org files
+  ;; (add-to-list 'org-latex-export-classes
+  ;;              ;; beamer class, for presentations
+  ;;              '("beamer"
+  ;;                "\\documentclass[11pt]{beamer}\n
+  ;;       \\mode<{{{beamermode}}}>\n
+  ;;       \\usetheme{{{{beamertheme}}}}\n
+  ;;       \\usecolortheme{{{{beamercolortheme}}}}\n
+  ;;       \\beamertemplateballitem\n
+  ;;       \\setbeameroption{show notes}\n
+  ;;       \\usepackage[utf8]{inputenc}\n
+  ;;       \\usepackage[T1]{fontenc}\n
+  ;;       \\usepackage{hyperref}\n
+  ;;       \\usepackage{color}\n
+  ;;       \\usepackage{verbatim}\n
+  ;;       \\newcommand{\\xvec}{\\mathbf{x}}\n
+  ;;       \\institute{{{{beamerinstitute}}}}\n
+  ;;       \\subject{{{{beamersubject}}}}\n"
+
+  ;;                ("\\section{%s}" . "\\section*{%s}")
+
+  ;;                ("\\begin{frame}[fragile]\\frametitle{%s}"
+  ;;                 "\\end{frame}"
+  ;;                 "\\begin{frame}[fragile]\\frametitle{%s}"
+  ;;                 "\\end{frame}")))
+
+  ;; ;; letter class, for formal letters
+
+  ;; (add-to-list 'org-latex-export-classes
+  ;;              '("letter"
+  ;;                "\\documentclass[11pt]{letter}\n
+  ;;                 \\usepackage[utf8]{inputenc}\n
+  ;;                 \\usepackage[T1]{fontenc}\n
+  ;;                 \\usepackage{color}"
+
+  ;;                ("\\section{%s}" . "\\section*{%s}")
+  ;;                ("\\subsection{%s}" . "\\subsection*{%s}")
+  ;;                ("\\subsubsection{%s}" . "\\subsubsection*{%s}")
+  ;;                ("\\paragraph{%s}" . "\\paragraph*{%s}")
+  ;;                ("\\subparagraph{%s}" . "\\subparagraph*{%s}")))
+
+  ;; (org-babel-do-load-languages
+  ;;  'org-babel-load-languages
+  ;;  '((emacs-lisp . nil)
+  ;;    (python . t))))
+  )
 
 ;; (use-package org-projectile
 ;;   :bind (("C-c n p" . org-projectile-project-todo-completing-read)
