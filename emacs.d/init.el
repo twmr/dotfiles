62161f6c703585fcc5ff85bfe8858a48546a6f8c
Thomas Hisch <thomas.hisch@ims.co.at>
2022-04-24 12:30:25 +0200

    Initial mu4e config

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -2841,3 +2841,230 @@ comments from CI tools."
 (put 'downcase-region 'disabled nil)
 (setq org-image-actual-width nil)
 (put 'dired-find-alternate-file 'disabled nil)
+
+
+(use-package mu4e
+  :disabled t
+  ;; :defer 20 ; Wait until 20 seconds after startup
+  ;; :ensure nil ;; because this package is not on melpa
+  :load-path "/usr/share/emacs/site-lisp/mu4e"
+  :config
+  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+  ;; key bindings
+  ;; - show source of mail?
+  ;; - open mail in firefox
+  ;;
+
+  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+  ;; resources
+  ;; https://github.com/munen/emacs.d/#mu4e
+
+  ;; jira mail types can be changed to text (default html)
+
+  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+
+
+  ;; Load org-mode integration
+  (require 'org-mu4e)
+
+  ;; Refresh mail using isync every 10 minutes
+  (setq mu4e-update-interval (* 10 60))
+  (setq mu4e-get-mail-command "offlineimap")
+
+  ;; Use Ivy for mu4e completions (maildir folders, etc)
+  (setq mu4e-completing-read-function #'ivy-completing-read)
+
+  ;; Make sure that moving a message (like to Trash) causes the
+  ;; message to get a new file name.  This helps to avoid the
+  ;; dreaded "UID is N beyond highest assigned" error.
+  ;; See this link for more info: https://stackoverflow.com/a/43461973
+  (setq mu4e-change-filenames-when-moving t)
+
+  ;; Set up contexts for email accounts
+  (setq mu4e-contexts
+        `(,(make-mu4e-context
+            :name "IMS"
+            ;; :match-func (lambda (msg) (when msg
+            ;;                             (string-prefix-p "/Fastmail" (mu4e-message-field msg :maildir))))
+            :vars '(
+                    (user-full-name . "Thomas Hisch")
+                    (user-mail-address . (concat "thomas.hisch"
+                                                 "@"
+                                                 "ims.co.at"))
+                    (mu4e-sent-folder . "/Sent Items")
+                    (mu4e-trash-folder . "/Deleted Items")
+                    (mu4e-drafts-folder . "/Drafts")
+                    (mu4e-refile-folder . "/Archives")
+                    (mu4e-sent-messages-behavior . sent)
+                    ))
+          ))
+
+  ;; what if there is only one context???
+  (setq mu4e-context-policy 'pick-first)
+
+  ;; Prevent mu4e from permanently deleting trashed items
+  ;; This snippet was taken from the following article:
+  ;; http://cachestocaches.com/2017/3/complete-guide-email-emacs-using-mu-and-/
+  (defun remove-nth-element (nth list)
+    (if (zerop nth) (cdr list)
+      (let ((last (nthcdr (1- nth) list)))
+        (setcdr last (cddr last))
+        list)))
+  (setq mu4e-marks (remove-nth-element 5 mu4e-marks))
+  (add-to-list 'mu4e-marks
+               '(trash
+                 :char ("d" . "▼")
+                 :prompt "dtrash"
+                 :dyn-target (lambda (target msg) (mu4e-get-trash-folder msg))
+                 :action (lambda (docid msg target)
+                           (mu4e~proc-move docid
+                                           (mu4e~mark-check-target target) "-N"))))
+
+  ;; Display options
+  (setq mu4e-view-show-images t)
+  (setq mu4e-view-show-addresses 't)
+
+  ;; html mails (see msg2pdf tool, which is part of the mu4e github repo)
+  ;; https://200ok.ch/posts/2020-05-27_using_emacs_and_mu4e_for_emails_even_with_html.html
+  ;; what's the command for opening html mails in firefox???
+
+  (mu4e-column-faces-mode)
+  (setq mu4e-headers-fields
+        '((:human-date    .   12)
+          (:flags         .    6)
+          ;; I don't want to see mailing list info
+          ;; (:mailing-list  .   10)
+          (:from          .   22)
+          (:subject       .   nil)))
+
+  ;; I don't want to see threads
+  (setq mu4e-headers-show-threads nil)
+
+  ;; Composing mail
+  (setq mu4e-compose-dont-reply-to-self t)
+
+  ;; Use mu4e for sending e-mail
+  (setq mail-user-agent 'mu4e-user-agent)
+  (setq message-send-mail-function 'smtpmail-send-it)
+
+  ;; Signing messages (use mml-secure-sign-pgpmime)
+  ;; (setq mml-secure-openpgp-signers '("53C41E6E41AAFE55335ACA5E446A2ED4D940BF14"))
+
+  ;; (See the documentation for `mu4e-sent-messages-behavior' if you have
+  ;; additional non-Gmail addresses and want assign them different
+  ;; behavior.)
+
+  ;; setup some handy shortcuts
+  ;; you can quickly switch to your Inbox -- press ``ji''
+  ;; then, when you want archive some messages, move them to
+  ;; the 'All Mail' folder by pressing ``ma''.
+  (setq mu4e-maildir-shortcuts
+        '(("/INBOX"       . ?i)
+          ;; ("/Fastmail/Lists/*"     . ?l)
+          ("/Sent Items"   . ?s)
+          ("/Deleted Items"       . ?t)))
+
+  (add-to-list 'mu4e-bookmarks
+               (make-mu4e-bookmark
+                :name "All Inboxes"
+                :query "maildir:/INBOX"
+                :key ?i))
+
+  (add-to-list 'mu4e-bookmarks
+               (make-mu4e-bookmark
+                :name "Filtered Inboxes"
+                :query (string-join '(
+                                      "(maildir:/INBOX"
+                                      "NOT from:/gerrit@.*ims.co.at/"
+                                      ;; "NOT from:gerrit@srv-vct-02.ims.co.at"
+                                      "NOT from:jira.rnd@ims.co.at"
+                                      "NOT from:jira.service@ims.co.at"
+                                      "NOT from:mattermost@ims.co.at"
+                                      "NOT from:quarantine@ims.co.at)"
+                                      ) ") AND (")
+                :key ?f))
+
+
+  (add-to-list 'mu4e-bookmarks
+               (make-mu4e-bookmark
+                :name "Jira/Gerrit"
+                :query (concat "maildir:/INBOX OR ("
+                               (string-join '(
+                                              "from:/gerrit@.*ims.co.at/"
+                                              "from:jira.rnd@ims.co.at"
+                                              "from:jira.service@ims.co.at"
+                                              )
+                                            ") OR (")
+                               ")")
+                :key ?j))
+
+
+  ;; (plist-get (nth 1 mu4e-bookmarks) :name)
+  ;; (plist-get (nth 1 mu4e-bookmarks) :query)
+  ;; (shell-command-to-string (concat "mu find --reverse -n 20 '" (plist-get (nth 1 mu4e-bookmarks) :query) "'"))
+  ;; (mu4e~headers-search-execute (plist-get (nth 1 mu4e-bookmarks) :query) t)
+
+
+  (add-to-list 'mu4e-bookmarks
+               (make-mu4e-bookmark
+                :name "Filtered Today"
+                :query (string-join '(
+                                      "maildir:/INBOX"
+                                      "NOT from:/gerrit@.*ims.co.at/"
+                                      ;; "NOT from:gerrit@srv-vct-02.ims.co.at"
+                                      "NOT from:jira.rnd@ims.co.at"
+                                      "NOT from:jira.service@ims.co.at"
+                                      "NOT from:mattermost@ims.co.at"
+                                      "NOT from:quarantine@ims.co.at"
+                                      "date:today..now"
+                                      )  " AND ")
+                :key ?n))
+
+  ;; Close mu4e without asking.
+  (setq mu4e-confirm-quit nil)
+
+  ;; don't keep message buffers around
+  (setq message-kill-buffer-on-exit t)
+
+  ;; always prefer text/plain version over html version
+  (setq mu4e-view-html-plaintext-ratio-heuristic most-positive-fixnum)
+
+  ;; Press "a" (actions) then "v" in the current message to open it in the default
+  ;; web browser.
+  ;; already part of mu4e
+  ;; (add-to-list 'mu4e-view-actions '("ViewInBrowser" . mu4e-action-view-in-browser) t)
+
+  ;; (defun mu4e-action-view-in-browser-webkit (msg)
+  ;;   (let ((url (concat "file://" (mu4e~write-body-to-html msg))))
+  ;;     (xwidget-webkit-browse-url url)))
+
+  (defun mu4e-action-view-in-xwidget (msg)
+    "Show current MSG in an embedded xwidget, if available."
+    (unless (fboundp 'xwidget-webkit-browse-url)
+      (mu4e-error "No xwidget support available"))
+    (let ((browse-url-browser-function
+           (lambda (url &optional _rest)
+             (xwidget-webkit-browse-url url))))
+      (mu4e-action-view-in-browser msg)))
+
+  (add-to-list 'mu4e-view-actions
+               '("web browser" .  mu4e-action-view-in-xwidget)) ;;mu4e-action-view-in-browser-webkit)))
+
+
+  ;; (setq thi::mu4e-inbox-query
+  ;;       "from:/gerrit@.*ims.co.at/ flag:unread")
+
+  (defun thi::mail-gerrit ()
+    (interactive)
+    (mu4e-headers-search "from:/gerrit@.*ims.co.at/ flag:unread"))
+
+  ;; (dw/leader-key-def
+  ;;   "m"  '(:ignore t :which-key "mail")
+  ;;   "mm" 'mu4e
+  ;;   "mc" 'mu4e-compose-new
+  ;;   "mi" 'thi::go-to-inbox
+  ;;   "ms" 'mu4e-update-mail-and-index)
+
+  ;; Start mu4e in the background so that it syncs mail periodically
+  (mu4e t))
